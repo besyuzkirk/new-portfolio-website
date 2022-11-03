09e56cefcaaa183c176072faea2b81e1327baaa4
Ahmet Karademir <ahmet.karademir@boun.edu.tr>
2022-11-04 01:09:12 +0300

    Portfolio-6 image-bug-fix

--- src/dummy_datas/ProjectData.js
+++ src/dummy_datas/ProjectData.js
@@ -1,30 +1,30 @@
 export const PROJECTS = [
     {
         projectName : "Movie Director Api / NodeJS",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/node-movie-api.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/node-movie-api.png'
     },
     {
         projectName : "Expense Tracker / Flutter",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/expensetracker.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/expensetracker.png'
     },
     {
         projectName : "Password Generator / Android SDK",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/password-generator.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/password-generator.png'
     },
     {
         projectName : "E-commerce App / Django",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/djangomvc.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/djangomvc.png'
     },
     {
         projectName : "Dice Roller / Android SDK",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/dice.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/dice.png'
     },
     {
         projectName : "Personel Web Page / Vanilla Javascript",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/webpage.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/webpage.png'
     },
     {
         projectName : "To-Do App / Vanilla Javascript",
-        projectImg: process.env.PUBLIC_URL + 'images/portfolio/todo.png'
+        projectImg: process.env.PUBLIC_URL + '/images/portfolio/todo.png'
     }
 ]
