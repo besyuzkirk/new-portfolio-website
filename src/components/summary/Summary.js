ebb5c933a0284469d143a49ce8f41e0c9e282ec2
Ahmet Karademir <ahmet.karademir@boun.edu.tr>
2022-11-05 22:29:14 +0300

    Portfolio-8 responsivity-fixes

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,25 +1,54 @@
-import React from 'react'
-import './Summary.css'
-import {  FiInstagram ,FiLinkedin , FiYoutube , FiGithub  } from "react-icons/fi";
+import React from "react";
+import "./Summary.css";
+import { FiInstagram, FiLinkedin, FiYoutube, FiGithub } from "react-icons/fi";
 import { MdOutlineBusinessCenter } from "react-icons/md";
 import { SiMedium } from "react-icons/si";
 
 function Summary() {
   return (
-    <div className='summary'>
-        <h1>Hello!</h1>
-        <h2>I'm Ahmet Karademir</h2>
-        <p>I am a highly motivated and ambitious MIS student at Boğaziçi University with strong communication skills and learning ability. I am very serious about my career and I am driven to be the best at what I do. You can check out my accounts and say hello.</p>
-        <ul>
-          <li><a href='/'><FiGithub/></a></li>
-          <li><a href='/'><FiLinkedin/></a></li>
-          <li><a href='/'><MdOutlineBusinessCenter/></a></li>
-          <li><a href='/'><SiMedium/></a></li>
-          <li><a href='/'><FiYoutube/></a></li>
-          <li><a href='/'><FiInstagram/></a></li>
-        </ul>
+    <div className="summary">
+      <h1>Hello!</h1>
+      <h2>I'm Ahmet Karademir</h2>
+      <p>
+        I am a highly motivated and ambitious MIS student at Boğaziçi University
+        <span className="summary-mobile">with strong communication skills and learning ability</span>. I am very serious
+        about my career and I am driven to be the best at what I do. You can
+        check out my accounts and say hello.
+      </p>
+      <ul>
+        <li>
+          <a href="/">
+            <FiGithub />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <FiLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <MdOutlineBusinessCenter />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <SiMedium />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <FiYoutube />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <FiInstagram />
+          </a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Summary
+export default Summary;
--- src/sections/underwater/UnderWater.js
+++ src/sections/underwater/UnderWater.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
+import React from "react";
 import "aos/dist/aos.css";
 import "./UnderWater.css";
 import { HiOutlineChevronDoubleDown } from "react-icons/hi";
@@ -15,29 +14,41 @@ import ExperienceLayout from "../../layouts/experience/ExperienceLayout";
 import { STORY_DATA } from "../../dummy_datas/StoryData";
 import Projects from "../../layouts/project/Projects";
 import Contact from "../../components/contact/Contact";
+import { Link } from "react-scroll";
 
 function UnderWater() {
- 
-
   return (
     <div className="header-section">
       <div className="scroll-down-icon">
         <p>Deep Dive And Learn More About Me</p>
-        <a href="">
-          <HiOutlineChevronDoubleDown size={50} />
-        </a>
+        <Link
+          className="scroll-link"
+          activeClass="active"
+          to="scroll-swipe"
+          spy={true}
+          smooth={true}
+          offset={-200}
+          duration={1000}
+        >
+          <HiOutlineChevronDoubleDown className="scroll-down-icon-svg" />
+        </Link>
       </div>
+      <span id="scroll-swipe"></span>
       <Box
         left={<p>{STORY_DATA}</p>}
         right={<img className="illustration" src={STORY_ILLUSTRATION}></img>}
       />
       <Box
         right={<ExperienceLayout title="My Experiences" />}
-        left={<img className="illustration" src={EXPERIENCE_ILLUSTRATION}></img>}
+        left={
+          <img className="illustration" src={EXPERIENCE_ILLUSTRATION}></img>
+        }
       />
       <Box
         title="My Certifications"
-        right={<img className="illustration" src={CERTIFICATION_ILLUSTRATION}></img>}
+        right={
+          <img className="illustration" src={CERTIFICATION_ILLUSTRATION}></img>
+        }
         left={<CertificationLayout title="My Certifications" />}
       />
       <Projects />
