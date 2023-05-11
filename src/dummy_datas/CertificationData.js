9e193f58c5393ed56b416c2d806f0facb4af900c
Ahmet Karademir <ahmet.karademir@boun.edu.tr>
2023-05-11 21:56:19 +0300

    UpdateCompanyLogos

--- src/dummy_datas/CertificationData.js
+++ src/dummy_datas/CertificationData.js
@@ -1,19 +1,19 @@
 export const CERTIFICATION_DATA = [
     {
         id : "c1",
-        certImg : "https://media-exp1.licdn.com/dms/image/C560BAQE88xCsONDULQ/company-logo_200_200/0/1618231291419?e=1674691200&v=beta&t=jmsnEQc_FLOzk8JUMPqhXk4CybRuLedO2zgxSQfdgyA",
+        certImg : "https://media.licdn.com/dms/image/C560BAQE88xCsONDULQ/company-logo_200_200/0/1618231291419?e=1691625600&v=beta&t=RR_Gk0ct4nZbMzbGHgAqCBa3OzEneb9B7fEVrx4h48Q",
         certTitle : "Developing Cross-Platform Web Apps with ASP.NET Core",
         certGiver : "Microsoft"
     },
     {
         id : "c2",
-        certImg : "https://media-exp1.licdn.com/dms/image/C4E0BAQFgocdTGHthDA/company-logo_200_200/0/1581351014172?e=1674691200&v=beta&t=yA2PrN_h9Cuxw4itVdKT8imK8Q-3-rbfT7-vV_W-nDc",
+        certImg : "https://media.licdn.com/dms/image/C4E0BAQFgocdTGHthDA/company-logo_200_200/0/1581351014172?e=1691625600&v=beta&t=zdfDadg4RguuAsZqa7_W68IyJHm9XC_2kRk-5mn3kTo",
         certTitle : "Introduction to Cyber Security",
         certGiver : "Cisco Networking Academy"
     },
     {
         id : "c3",
-        certImg : "https://media-exp1.licdn.com/dms/image/C4D0BAQGexnfBxeEG-g/company-logo_200_200/0/1608039227697?e=1674691200&v=beta&t=qHflb35CZzwD7_hZsMWwhzbJl79t6bSMqp6q0MMMmiQ",
+        certImg : "https://media.licdn.com/dms/image/C4D0BAQGexnfBxeEG-g/company-logo_200_200/0/1608039227697?e=1691625600&v=beta&t=4v11u2I_4jvfRPnlFnbcnQEuArtUPM57X0lCdBtV6AM",
         certTitle : "Amazon Going-Cloud Native",
         certGiver : "Coursera"
     },
--- src/dummy_datas/ExperienceData.js
+++ src/dummy_datas/ExperienceData.js
@@ -4,20 +4,28 @@ export const DUMMY_EXPERIENCE = [
         firmImageSrc: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEHhQAWFpWiTA/company-logo_200_200/0/1543389570190?e=1672876800&v=beta&t=Q36oXnKKU2Ie4z2nGOwb2LQ5v8MPItCk6nomrfq0yYo',
         jobTitle: "Software Development Project Student",
         firmTitle: "BSH Turkey · Part Time",
-        jobDate: "Agu 2022 - Present",
+        jobDate: "Jan 2022 - Present",
         jobPlace: "Istanbul, Turkey"
     },
     {
         id: 'e2',
-        firmImageSrc: 'https://media-exp1.licdn.com/dms/image/C4D0BAQHRWfu4JpddFg/company-logo_200_200/0/1660732431620?e=1672876800&v=beta&t=mZqRyb8kx24SZRuPH89uCilXd5j7SjbwykkvmkQ2mNE',
+        firmImageSrc: 'https://media.licdn.com/dms/image/D4D0BAQEnnBoR-CAO_w/company-logo_100_100/0/1680254075685?e=1691625600&v=beta&t=oTwWj3r9W4uonko4ba0VfikuKMNro--x_XxwsoXN6LQ',
+        jobTitle: "Software Development Project Student",
+        firmTitle: "BSH Turkey · Part Time",
+        jobDate: "Agu 2022 - Dec 2022 · 4 months",
+        jobPlace: "Istanbul, Turkey"
+    },
+    {
+        id: 'e3',
+        firmImageSrc: 'https://media.licdn.com/dms/image/C4D0BAQG9r50DSfZBUw/company-logo_100_100/0/1679386631096?e=1691625600&v=beta&t=hR1mOSgz2KuZMQJSbNsiv-Bdrxw62HSAnU9Azq8kcXI',
         jobTitle: "Backend Developer Intern",
         firmTitle: "Birlesik Ödeme · Internship",
         jobDate: "Oct 2021 - Agu 2022 · 10 months",
         jobPlace: "Istanbul, Turkey"
     },
     {
-        id: 'e3',
-        firmImageSrc: 'https://media-exp1.licdn.com/dms/image/C4D0BAQFiBJn3_SFnDQ/company-logo_200_200/0/1622372384333?e=1672876800&v=beta&t=HIcfR6prOtR51pW3Ywmc7ea6fvjR6f_hi0H1CGHWsVM',
+        id: 'e4',
+        firmImageSrc: 'https://media.licdn.com/dms/image/C4D0BAQFiBJn3_SFnDQ/company-logo_200_200/0/1622372384333?e=1691625600&v=beta&t=upfN2m8q3DTMn2a0ODhjERV8iyIeJFy0D5LtKWNlERE',
         jobTitle: "Web Developer Intern",
         firmTitle: "MG UrbanFarmDe GmbH · Internship",
         jobDate: "Jul 2021 - Oct 2021 · 3 months",
